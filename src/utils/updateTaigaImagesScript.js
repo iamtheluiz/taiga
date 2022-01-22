77bd89d1f274b04c0cf598b8507336af8c724249
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-01-22 18:45:25 -0300

    feat: create script to update taigaImages

---
+++ src/utils/updateTaigaImagesScript.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const assetsFolder = fs.readdirSync(path.join(__dirname, '..', 'assets'));
+let fileString = 'export const taigaImages: any = {\n';
+
+for (let index = 0; index < assetsFolder.length; index++) {
+  const asset = assetsFolder[index];
+  
+  fileString += `"${path.parse(asset).name}": require("../assets/${asset}"),\n`
+}
+
+fileString += '}';
+
+fs.writeFileSync(path.join(__dirname, 'taigaImages.tsx'), fileString)
+
+console.log('Complete!');
