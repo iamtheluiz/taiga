1127674a52f9ace1e556eb1db090e26ee356b881
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-01-29 20:57:59 -0300

    feat(recognition): create logs

--- electron/lib/Recognition.ts
+++ electron/lib/Recognition.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import { spawn, ChildProcessWithoutNullStreams } from 'child_process'
 import { Command } from './Command'
+import { log } from '../logger'
 
 const executablePath =
   process.env.NODE_ENV === 'production'
@@ -16,36 +17,52 @@ const executablePath =
         'TaigaRecognition.exe'
       )
 
+const recognitionLog = log.scope('Recognition')
+
 export class Recognition {
   static ipc: ChildProcessWithoutNullStreams | null = null
   static isRecognizing: boolean = false
 
   static startRecognition() {
+    recognitionLog.debug('startRecognition - start')
+
     this.ipc = spawn(executablePath)
+    recognitionLog.debug('startRecognition - spawn Taiga Recognition')
+
     this.ipc.stdin.setDefaultEncoding('utf8')
+    recognitionLog.debug('startRecognition - set enconding')
 
     this.ipc.stdout.on('data', data => {
+      recognitionLog.debug('startRecognition - on data:', data)
       const out = data
         .toString()
         .split('Recognized text: ')[1]
         .replace(/(\r\n|\n|\r)/gm, '')
-      console.log(`You: ${out}`)
+      recognitionLog.info('startRecognition - recognized command name:', out)
 
       Command.executeCommand(out)
     })
+    recognitionLog.debug('startRecognition - define on data receive')
 
     this.isRecognizing = true
+    recognitionLog.debug('startRecognition - end')
   }
 
   static stopRecognition() {
+    recognitionLog.debug('stopRecognition - start')
     this.ipc?.kill()
+    recognitionLog.debug('stopRecognition - kill process')
     this.ipc = null
+    recognitionLog.debug('stopRecognition - define process as null')
 
     this.isRecognizing = false
+    recognitionLog.debug('stopRecognition - end')
   }
 
   static resetRecognition() {
+    recognitionLog.debug('resetRecognition - start')
     this.stopRecognition()
     this.startRecognition()
+    recognitionLog.debug('resetRecognition - end')
   }
 }
