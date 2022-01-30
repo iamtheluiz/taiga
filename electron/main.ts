b85eaf69085c3b8ee47c4bb50e2dd8b6c9d74c8f
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-01-29 22:35:42 -0300

    refactor(communication): create class to handle ipcMain communications

--- electron/main.ts
+++ electron/main.ts
@@ -1,9 +1,9 @@
-import { app, BrowserWindow, dialog, ipcMain } from 'electron'
+import { app, BrowserWindow } from 'electron'
 import path from 'path'
 
 import { log } from './logger'
 
-import { Command } from './lib/Command'
+import { Communication } from './lib/Communication'
 import { Recognition } from './lib/Recognition'
 
 let mainWindow: BrowserWindow | null
@@ -41,85 +41,12 @@ function createWindow() {
   })
 }
 
-async function registerListeners() {
-  log.debug('Registering listeners')
-
-  const listenerLog = log.scope('Listener')
-  const senderLog = log.scope('Sender')
-
-  /**
-   * This comes from bridge integration, check bridge.ts
-   */
-  ipcMain.on('message', (_, message) => {
-    listenerLog.info('message: ', message)
-  })
-
-  ipcMain.on('open-dialog', async (_, message) => {
-    listenerLog.info('open dialog: ', message)
-    const selectedFile = await dialog.showOpenDialog({
-      properties: ['openFile'],
-    })
-
-    _.sender.send('open-dialog-response', selectedFile)
-    senderLog.info('open dialog response: ', selectedFile)
-  })
-
-  ipcMain.on('get-commands', (_, message) => {
-    listenerLog.info('get commands: ', message)
-
-    const commands = Command.getCommandList()
-
-    _.sender.send('update-commands', commands)
-    senderLog.info('update commands: ', commands)
-  })
-
-  ipcMain.on('add-new-command', (_, message) => {
-    listenerLog.info('add new command: ', message)
-
-    Command.addNewCommand(message.command)
-    const commands = Command.getCommandList()
-
-    _.sender.send('update-commands', commands)
-    senderLog.info('update commands: ', commands)
-
-    Recognition.resetRecognition()
-  })
-
-  ipcMain.on('remove-command', (_, message) => {
-    listenerLog.info('remove command: ', message)
-
-    Command.removeCommand(message.command)
-
-    const commands = Command.getCommandList()
-    _.sender.send('update-commands', commands)
-    senderLog.info('update commands: ', commands)
-
-    Recognition.resetRecognition()
-  })
-
-  ipcMain.on('taiga-recognition', (_, message) => {
-    if (message.action === 'turn-off') {
-      Recognition.stopRecognition()
-      _.sender.send('taiga-recognition-status', { isRecognizing: false })
-    } else if (message.action === 'turn-on') {
-      Recognition.startRecognition()
-      _.sender.send('taiga-recognition-status', { isRecognizing: true })
-    }
-  })
-
-  ipcMain.on('taiga-recognition-get-status', (_, message) => {
-    _.sender.send('taiga-recognition-status', {
-      isRecognizing: Recognition.isRecognizing,
-    })
-  })
-}
-
 app
   .on('ready', createWindow)
   .whenReady()
   .then(() => {
     log.debug('App ready')
-    registerListeners()
+    Communication.registerListeners()
   })
   .catch(log.catchErrors)
 
