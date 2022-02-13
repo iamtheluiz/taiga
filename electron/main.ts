2cf1a778cd9dedd8669842a8454762f6a4d4961a
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-02-13 09:50:17 -0300

    refactor(electron): create Window lib

--- electron/main.ts
+++ electron/main.ts
@@ -1,51 +1,16 @@
 import { app, BrowserWindow } from 'electron'
-import path from 'path'
 
 import { log } from './logger'
 
 import { Communication } from './lib/Communication'
 import { Recognition } from './lib/Recognition'
 import { Command } from './lib/Command'
-
-let mainWindow: BrowserWindow | null
-
-declare const MAIN_WINDOW_WEBPACK_ENTRY: string
-declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
-
-const assetsPath =
-  process.env.NODE_ENV === 'production'
-    ? process.resourcesPath
-    : app.getAppPath()
+import { Window } from './lib/Window'
 
 Command.createCommandsFile()
 
-function createWindow() {
-  log.debug('Creating window')
-
-  mainWindow = new BrowserWindow({
-    icon: path.join(assetsPath, 'assets', 'icon.ico'),
-    width: 1100,
-    height: 700,
-    backgroundColor: '#191622',
-    frame: false,
-    webPreferences: {
-      enableRemoteModule: true,
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
-    },
-  })
-
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
-
-  mainWindow.on('closed', () => {
-    log.debug('Main window closed')
-    mainWindow = null
-  })
-}
-
 app
-  .on('ready', createWindow)
+  .on('ready', Window.createWindow)
   .whenReady()
   .then(() => {
     log.debug('App ready')
@@ -66,6 +31,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    Window.createWindow()
   }
 })
