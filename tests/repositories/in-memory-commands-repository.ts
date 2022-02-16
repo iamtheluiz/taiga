1ec46824a767af8213cc91200262d5c0e33998d3
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-02-16 20:33:57 -0300

    feat: implement InMemoryCommandsRepository

---
+++ tests/repositories/in-memory-commands-repository.ts
@@ -0,0 +1,30 @@
+import { Command } from '../../electron/entities/Command'
+import { CommandsRepository } from '../../electron/repositories/CommandsRepository'
+
+export class InMemoryCommandsRepository implements CommandsRepository {
+  public commands: Command[] = []
+
+  async findById(id: string): Promise<Command | null> {
+    const command = this.commands.find(command => command.id === id)
+
+    if (!command) {
+      return null
+    }
+
+    return command
+  }
+
+  async findByName(name: string): Promise<Command | null> {
+    const command = this.commands.find(command => command.name === name)
+
+    if (!command) {
+      return null
+    }
+
+    return command
+  }
+
+  async save(command: Command): Promise<void> {
+    this.commands.push(command)
+  }
+}
