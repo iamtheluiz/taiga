26def9f745f386b5db17878e87990cbf6752ec1c
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-06-20 00:19:55 -0300

    feat(usecases): add logs to start and stop recognition usecases

--- electron/usecases/start-recognition.ts
+++ electron/usecases/start-recognition.ts
@@ -1,7 +1,11 @@
+import { log as logger } from '../lib/logger'
+
 import { CommandExecutionProvider } from '../providers/CommandExecutionProvider'
 import { RecognitionProvider } from '../providers/RecognitionProvider'
 import { CommandsRepository } from '../repositories/CommandsRepository'
 import { ExecuteCommand } from './execute-command'
+
+const log = logger.scope('StartRecognition')
 
 export class StartRecognition {
   // eslint-disable-next-line no-useless-constructor
@@ -12,15 +16,19 @@ export class StartRecognition {
   ) {}
 
   async execute() {
+    log.debug('Starting recognition')
     const executeCommand = new ExecuteCommand(
       this.commandsRepository,
       this.commandExecutionProvider
     )
 
     this.recognitionProvider.executeCommand = async (id: string) => {
+      log.debug('Defining on recognized action')
       executeCommand.execute(id)
     }
+
     const status = this.recognitionProvider.startRecognition()
+    log.info(`isRecognizing: ${status.isRecognizing}`)
 
     return status
   }
--- electron/usecases/stop-recognition.ts
+++ electron/usecases/stop-recognition.ts
@@ -1,16 +1,22 @@
+import { log as logger } from '../lib/logger'
+
 import { RecognitionProvider } from '../providers/RecognitionProvider'
+
+const log = logger.scope('StopRecognition')
 
 export class StopRecognition {
   // eslint-disable-next-line no-useless-constructor
   constructor(private recognitionProvider: RecognitionProvider) {}
 
   async execute() {
+    log.debug('Stopping recognition')
     const status = this.recognitionProvider.stopRecognition()
 
     if (!status) {
       throw new Error('Error stopping the recognition')
     }
 
+    log.info(`isRecognizing: ${status.isRecognizing}`)
     return status
   }
 }
